98869c1056eaafac58f871671c51d9e32dbe93e5
qiujiandong <1335521934@qq.com>
2025-03-05 00:10:43 +0800

    feat: add clang-format
    Signed-off-by: qiujiandong <1335521934@qq.com>

--- init.lua
+++ init.lua
@@ -715,6 +715,7 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'clang-format', -- Used to format C/C++ code
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
