ce5264c620387cc604d278cd5ac7bc5f4e5f62e1
qiujiandong <1335521934@qq.com>
2025-03-05 00:13:00 +0800

    feat: add bufferline.nvim
    Signed-off-by: qiujiandong <1335521934@qq.com>

---
+++ lua/custom/plugins/bufferline.lua
@@ -0,0 +1,21 @@
+return {
+  'akinsho/bufferline.nvim',
+  config = function()
+    require('bufferline').setup {
+      options = {
+        offsets = {
+          {
+            filetype = 'neo-tree',
+            text = 'Neo-tree',
+            highlight = 'Directory',
+            text_align = 'left',
+          },
+        },
+      },
+    }
+    vim.keymap.set('n', '<M-b>', '<cmd>BufferLinePick<CR>')
+    vim.keymap.set('n', '<M-B>', '<cmd>BufferLinePickClose<CR>')
+    vim.keymap.set('n', '<M-l>', '<cmd>BufferLineCycleNext<CR>')
+    vim.keymap.set('n', '<M-h>', '<cmd>BufferLineCyclePrev<CR>')
+  end,
+}
