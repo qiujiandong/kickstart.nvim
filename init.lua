7ee4d68c34dfbb565618afc660956dec2e64a30c
qiujiandong <qiujiandong@nucleisys.com>
2025-03-19 10:08:30 +0800

    config: add `yamlfmt` to format yaml files
    Signed-off-by: qiujiandong <qiujiandong@nucleisys.com>

--- init.lua
+++ init.lua
@@ -677,6 +677,7 @@ require('lazy').setup({
       vim.list_extend(ensure_installed, {
         -- You can add other tools here that you want Mason to install
         'clang-format', -- Used to format C/C++ code
+        'yamlfmt', -- Used to format yaml file
       })
 
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
@@ -720,6 +721,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        yaml = { 'yamlfmt' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
