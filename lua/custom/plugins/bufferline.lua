773e817e9708a2ae4d12abb7443f9e72e84dd583
qiujiandong <qiujiandong@nucleisys.com>
2025-03-21 16:56:53 +0800

    config: bind key <leader>bD to delete ohter buffer
    Signed-off-by: qiujiandong <qiujiandong@nucleisys.com>

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -17,5 +17,6 @@ return {
     vim.keymap.set('n', '<M-B>', '<cmd>BufferLinePickClose<CR>')
     vim.keymap.set('n', '<M-l>', '<cmd>BufferLineCycleNext<CR>')
     vim.keymap.set('n', '<M-h>', '<cmd>BufferLineCyclePrev<CR>')
+    vim.keymap.set('n', '<leader>bD', '<cmd>BufferLineCloseOthers<CR>', { silent = true, desc = '[D]elete other [B]uffer' })
   end,
 }
