026942247086cba334f6c681e766a8edbc7c18b8
qiujiandong <qiujiandong@nucleisys.com>
2025-03-27 20:41:03 +0800

    config: update codeium config
    Signed-off-by: qiujiandong <qiujiandong@nucleisys.com>

--- lua/custom/plugins/codeium.lua
+++ lua/custom/plugins/codeium.lua
@@ -2,6 +2,20 @@ return {
   'Exafunction/codeium.vim',
   event = 'BufEnter',
   config = function()
-    vim.g.codeium_manual = true
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = 'python',
+      callback = function()
+        vim.g.codeium_manual = false
+      end,
+    })
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = { 'c', 'cpp', 'rust', 'lua', 'rst' },
+      callback = function()
+        vim.g.codeium_manual = true
+      end,
+    })
+    -- <C-]> Dismiss the current suggestion.
+    -- <M-]> Cycle to the next suggestion.
+    -- <M-[> Cycle to the previous suggestion.
   end,
 }
