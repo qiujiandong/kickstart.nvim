2151f2f5998a99a426ad9a1f60c8b65f979210c1
qiujiandong <qiujiandong@nucleisys.com>
2025-03-27 20:41:43 +0800

    config: add debug plugins
    Signed-off-by: qiujiandong <qiujiandong@nucleisys.com>

--- init.lua
+++ init.lua
@@ -751,7 +751,6 @@ require('lazy').setup({
         'lua-language-server',
         'markdownlint',
         'pyright',
-        'ruff', -- python linter
         'stylua', -- Used to format Lua code
         'yamlfmt', -- Used to format yaml file
       })
@@ -1022,7 +1021,7 @@ require('lazy').setup({
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
-  -- require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.debug',
   require 'kickstart.plugins.indent_line',
   require 'kickstart.plugins.lint',
   require 'kickstart.plugins.autopairs',
