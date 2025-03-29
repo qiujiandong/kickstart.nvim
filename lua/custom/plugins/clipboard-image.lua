032e71163eab0493d17287b2ab8aa10c5cc36040
qiujiandong <qiujiandong1998@gmail.com>
2025-03-29 22:47:16 +0800

    config: paste image in sphinx project
    Signed-off-by: qiujiandong <qiujiandong1998@gmail.com>

--- lua/custom/plugins/clipboard-image.lua
+++ lua/custom/plugins/clipboard-image.lua
@@ -8,6 +8,14 @@ return {
       use_absolute_path = false,
       relative_to_current_file = true,
     },
+    markdown = {
+      affix = '![](%s)',
+    },
+    rst = {
+      img_dir = { 'source', 'images' },
+      img_dir_txt = '/images',
+      affix = '%s',
+    },
   },
   -- NOTE: add patch with:
   -- curl -L https://github.com/ekickx/clipboard-image.nvim/pull/48.diff | git apply -
