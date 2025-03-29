f955affc44f3bc2078cec6c14b2363e5eaf87274
qiujiandong <qiujiandong1998@gmail.com>
2025-03-29 22:51:02 +0800

    mason: add rstcheck
    Signed-off-by: qiujiandong <qiujiandong1998@gmail.com>

--- init.lua
+++ init.lua
@@ -710,6 +710,7 @@ require('lazy').setup({
         'lua-language-server',
         'markdownlint',
         'pyright',
+        'rstcheck',
         'stylua', -- Used to format Lua code
         'yamlfmt', -- Used to format yaml file
       })
