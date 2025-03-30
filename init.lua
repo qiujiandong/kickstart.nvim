bb2b9730ed80439d4612742353742a72bdd8eb99
qiujiandong <qiujiandong1998@gmail.com>
2025-03-30 13:42:58 +0800

    config: add json formatter
    Signed-off-by: qiujiandong <qiujiandong1998@gmail.com>

--- init.lua
+++ init.lua
@@ -709,6 +709,7 @@ require('lazy').setup({
         'clang-format', -- Used to format C/C++ code
         'clangd',
         'isort', -- python formatter
+        'jq',
         'lua-language-server',
         'markdownlint',
         'pyright',
@@ -764,7 +765,7 @@ require('lazy').setup({
         -- rust = { 'rustfmt' },
         -- Conform can also run multiple formatters sequentially
         python = { 'isort', 'black' },
-        --
+        json = { 'jq' },
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
       },
