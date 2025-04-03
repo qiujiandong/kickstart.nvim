7adfcad22616d1750fd73534b3aa563886f7ab6c
qiujiandong <qiujiandong@nucleisys.com>
2025-04-03 18:23:28 +0800

    config: use treesitter to create fold
    Signed-off-by: qiujiandong <qiujiandong@nucleisys.com>

--- init.lua
+++ init.lua
@@ -164,6 +164,10 @@ vim.o.scrolloff = 10
 -- See `:help 'confirm'`
 vim.o.confirm = true
 
+vim.opt.foldmethod = 'expr'
+vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+vim.opt.foldlevel = 99
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
