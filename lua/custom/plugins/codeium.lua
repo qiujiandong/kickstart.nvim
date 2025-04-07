1952f4ded8fa4c49548bf545098300027c773ec6
qiujiandong <qiujiandong1998@gmail.com>
2025-04-07 21:49:08 +0800

    config: enable codeium for all filetype as default
    Signed-off-by: qiujiandong <qiujiandong1998@gmail.com>

--- lua/custom/plugins/codeium.lua
+++ lua/custom/plugins/codeium.lua
@@ -2,18 +2,18 @@ return {
   'Exafunction/codeium.vim',
   event = 'BufEnter',
   config = function()
-    vim.api.nvim_create_autocmd('FileType', {
-      pattern = 'python',
-      callback = function()
-        vim.g.codeium_manual = false
-      end,
-    })
-    vim.api.nvim_create_autocmd('FileType', {
-      pattern = { 'c', 'cpp', 'rust', 'lua', 'rst' },
-      callback = function()
-        vim.g.codeium_manual = true
-      end,
-    })
+    -- vim.api.nvim_create_autocmd('FileType', {
+    --   pattern = 'python',
+    --   callback = function()
+    --     vim.g.codeium_manual = false
+    --   end,
+    -- })
+    -- vim.api.nvim_create_autocmd('FileType', {
+    --   pattern = { 'c', 'cpp', 'rust', 'lua', 'rst' },
+    --   callback = function()
+    --     vim.g.codeium_manual = true
+    --   end,
+    -- })
     -- <C-]> Dismiss the current suggestion.
     -- <M-]> Cycle to the next suggestion.
     -- <M-[> Cycle to the previous suggestion.
