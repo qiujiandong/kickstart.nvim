a96c4a46cd8c235b15c7ae7efc20d495829fc996
qiujiandong <qiujiandong1998@gmail.com>
2025-04-07 21:48:23 +0800

    config: add sphinx-lint for rst lint
    Signed-off-by: qiujiandong <qiujiandong1998@gmail.com>

--- init.lua
+++ init.lua
@@ -713,6 +713,7 @@ require('lazy').setup({
         'markdownlint',
         'pyright',
         'rstcheck',
+        'sphinx-lint',
         'stylua', -- Used to format Lua code
         'yamlfmt', -- Used to format yaml file
       })
