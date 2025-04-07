cd0e49340ba7489f6fb392854dd09c84ddf1ca1f
qiujiandong <qiujiandong1998@gmail.com>
2025-04-07 21:51:21 +0800

    config: <leader>Q to quit all
    Signed-off-by: qiujiandong <qiujiandong1998@gmail.com>

--- init.lua
+++ init.lua
@@ -181,6 +181,14 @@ vim.keymap.set('n', '<leader>nh', '<cmd>nohlsearch<CR>', { desc = '[N]o [H]ighli
 -- Save file
 vim.keymap.set('n', '<leader>x', '<cmd>w<CR>', { desc = 'Save buffer' })
 
+vim.keymap.set('n', '<leader>Q', function()
+  vim.ui.input({ prompt = 'Quit all? [y/n](y): ' }, function(input)
+    if input == nil or input == '' or input:lower() == 'y' then
+      vim.cmd 'qa'
+    end
+  end)
+end, { desc = '[Q]uit All with Confirm' })
+
 -- Quit window
 -- vim.keymap.set('n', '<leader>q', '<cmd>q<CR>', { desc = '[Q]uit' })
 
