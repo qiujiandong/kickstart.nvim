3c6efef161a7063c2aed55e1b9d5714385f6463c
qiujiandong <qiujiandong@nucleisys.com>
2025-05-21 11:44:18 +0800

    config: update neotree configurations
    Signed-off-by: qiujiandong <qiujiandong@nucleisys.com>

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -10,21 +10,48 @@ return {
     'MunifTanjim/nui.nvim',
   },
   lazy = false,
-  keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
-  },
-  opts = {
-    filesystem = {
-      filtered_items = {
-        visiable = true,
-        hide_dotfiles = false,
-        hide_gitignored = false,
-      },
-      window = {
-        mappings = {
-          ['\\'] = 'close_window',
+  -- keys = {
+  --   { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+  -- },
+  -- opts = {
+  --   filesystem = {
+  --     filtered_items = {
+  --       visiable = true,
+  --       hide_dotfiles = false,
+  --       hide_gitignored = false,
+  --     },
+  --     window = {
+  --       width = 30,
+  --       position = 'left',
+  --     },
+  --   }
+  -- },
+  cmd = 'Neotree',
+  config = function()
+    require('neo-tree').setup {
+      event_handlers = {
+        {
+          event = 'file_opened',
+          handler = function()
+            require('neo-tree.command').execute { action = 'close' }
+          end,
         },
       },
-    },
-  },
+      filesystem = {
+        use_libuv_file_watcher = true,
+        filtered_items = {
+          visiable = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
+      },
+      window = {
+        width = 30,
+        position = 'left',
+      },
+    }
+
+    vim.keymap.set('n', '<TAB>', ':Neotree toggle<CR>', { desc = '[T]oggle Neotree', noremap = true, silent = true })
+    vim.keymap.set('n', '<leader>o', ':Neotree focus<CR>', { desc = '[O]pen Neotree', noremap = true, silent = true })
+  end,
 }
