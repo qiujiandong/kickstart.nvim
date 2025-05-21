57f0b0cf26f00d72c5da0ed91bf7dbc68da94b38
qiujiandong <qiujiandong@nucleisys.com>
2025-05-21 09:51:32 +0800

    config: disable editorconfig
    Signed-off-by: qiujiandong <qiujiandong@nucleisys.com>

--- init.lua
+++ init.lua
@@ -93,6 +93,9 @@ vim.g.maplocalleader = ' '
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
+-- Disable editorconfig
+vim.g.editorconfig = false
+
 -- [[ Setting options ]]
 -- See `:help vim.o`
 -- NOTE: You can change these options as you wish!
