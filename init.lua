faf423f596943fc2280ed7374dc861acc2a0c2a6
qiujiandong <qiujiandong@nucleisys.com>
2025-06-27 16:03:23 +0800

    config: use space to replace tab
    Signed-off-by: qiujiandong <qiujiandong@nucleisys.com>

--- init.lua
+++ init.lua
@@ -174,6 +174,21 @@ vim.opt.foldlevel = 99
 -- Save session with more options
 vim.opt.sessionoptions = { 'buffers', 'curdir', 'tabpages', 'winsize' }
 
+vim.o.expandtab = true
+vim.o.shiftwidth = 4
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'make',
+  callback = function()
+    vim.opt_local.expandtab = false -- use tab not space
+    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.softtabstop = 0
+  end,
+})
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
