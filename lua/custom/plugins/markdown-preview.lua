bab4b31ab3a9212972d768cf94493bc7c9e6a6bd
qiujiandong <qiujiandong@nucleisys.com>
2025-09-13 22:15:16 +0800

    config: add config to open browser in WSL
    Signed-off-by: qiujiandong <qiujiandong@nucleisys.com>

--- lua/custom/plugins/markdown-preview.lua
+++ lua/custom/plugins/markdown-preview.lua
@@ -3,6 +3,14 @@ return {
   event = 'VeryLazy',
   cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
   ft = { 'markdown' },
+  init = function()
+    vim.cmd [[
+    function! OpenBrowserWSL(url)
+      execute 'silent !wslview ' . shellescape(a:url)
+    endfunction
+    ]]
+    vim.g.mkdp_browserfunc = 'OpenBrowserWSL'
+  end,
   build = function()
     vim.fn['mkdp#util#install']()
   end,
