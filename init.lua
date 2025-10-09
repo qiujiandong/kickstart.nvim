96b7186ec87582b43dd2ed5575c2dbedf7790f63
qiujiandong <qiujiandong1998@gmail.com>
2025-10-09 23:17:34 +0800

    config: add C snippets to insert header comment
    Signed-off-by: qiujiandong <qiujiandong1998@gmail.com>

--- init.lua
+++ init.lua
@@ -844,12 +844,15 @@ require('lazy').setup({
           -- `friendly-snippets` contains a variety of premade snippets.
           --    See the README about individual language/framework/plugin snippets:
           --    https://github.com/rafamadriz/friendly-snippets
-          -- {
-          --   'rafamadriz/friendly-snippets',
-          --   config = function()
-          --     require('luasnip.loaders.from_vscode').lazy_load()
-          --   end,
-          -- },
+          {
+            'rafamadriz/friendly-snippets',
+            config = function()
+              require('luasnip.loaders.from_vscode').lazy_load()
+              require('luasnip.loaders.from_lua').lazy_load {
+                paths = '~/.config/nvim/lua/snippets',
+              }
+            end,
+          },
         },
         opts = {},
       },
