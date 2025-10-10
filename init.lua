cf92ea755d3975c721543552643934dfff6db325
qiujiandong <qiujiandong@nucleisys.com>
2025-10-10 09:27:18 +0800

    config: add javascript formatter config
    Signed-off-by: qiujiandong <qiujiandong@nucleisys.com>

--- init.lua
+++ init.lua
@@ -818,7 +818,7 @@ require('lazy').setup({
         python = { 'isort', 'black' },
         json = { 'jq' },
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascript = { 'prettier' },
       },
     },
   },
