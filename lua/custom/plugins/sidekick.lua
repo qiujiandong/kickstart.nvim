15bca0b0f9f8ac9783fa4ca556d5463419c6ab7d
qiujiandong <qiujiandong@nucleisys.com>
2026-03-03 17:56:21 +0800

    config: Update sidekick config to auto balance splict window
    Signed-off-by: qiujiandong <qiujiandong@nucleisys.com>

--- lua/custom/plugins/sidekick.lua
+++ lua/custom/plugins/sidekick.lua
@@ -1,5 +1,6 @@
 return {
-  'folke/sidekick.nvim',
+  'qiujiandong/sidekick.nvim',
+  branch = 'balance_split',
   opts = {
     cli = {
       mux = {
@@ -11,82 +12,60 @@ return {
   keys = {
     {
       '<c-g>',
-      function()
-        require('sidekick').nes_jump_or_apply()
-      end,
+      function() require('sidekick').nes_jump_or_apply() end,
       desc = 'Goto/Apply Next Edit Suggestion',
       mode = { 'n' },
     },
     {
       '<leader>aa',
-      function()
-        require('sidekick.cli').toggle()
-      end,
+      function() require('sidekick.cli').toggle() end,
       desc = 'Sidekick Toggle CLI',
     },
     {
       '<leader>ad',
-      function()
-        require('sidekick.cli').close()
-      end,
+      function() require('sidekick.cli').close() end,
       desc = 'Detach a CLI Session',
     },
     {
       '<leader>at',
-      function()
-        require('sidekick.cli').send { msg = '{this}' }
-      end,
+      function() require('sidekick.cli').send { msg = '{this}' } end,
       mode = { 'x', 'n' },
       desc = 'Send This',
     },
     {
       '<leader>af',
-      function()
-        require('sidekick.cli').send { msg = '{file}' }
-      end,
+      function() require('sidekick.cli').send { msg = '{file}' } end,
       desc = 'Send File',
     },
     {
       '<leader>ap',
-      function()
-        require('sidekick.cli').prompt()
-      end,
+      function() require('sidekick.cli').prompt() end,
       mode = { 'n', 'x' },
       desc = 'Sidekick Select Prompt',
     },
     {
       '<leader>ac',
-      function()
-        require('sidekick.cli').toggle { name = 'qwen', focus = false }
-      end,
+      function() require('sidekick.cli').toggle { name = 'qwen', focus = false } end,
       desc = 'Sidekick Toggle Qwen',
     },
     {
       '<leader>ae',
-      function()
-        require('sidekick.nes').enable()
-      end,
+      function() require('sidekick.nes').enable() end,
       desc = 'Enable NES',
     },
     {
       '<leader>aE',
-      function()
-        require('sidekick.nes').disable()
-      end,
+      function() require('sidekick.nes').disable() end,
       desc = 'Disable NES',
     },
     {
       '<leader>an',
-      function()
-        require('sidekick.nes').clear()
-      end,
+      function() require('sidekick.nes').clear() end,
       desc = 'Clear NES',
     },
     {
       '<leader>au',
-      function()
-        require('sidekick.nes').update()
-      end,
+      function() require('sidekick.nes').update() end,
       desc = 'Update NES',
     },
   },
